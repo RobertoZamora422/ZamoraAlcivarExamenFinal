28d677c00eb259d04d592f0b3e1f92da5d1d3ad7
RobertoZmora <robertozamora421@gmail.com>
2025-07-08 08:33:03 -0500

    Implementaciones en la carpeta Services y configuacion SQLite

---
+++ ZamoraAlcivarExamenFinal/Services/DatabaseService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using ZamoraAlcivarExamenFinal.Models;
+using System.Diagnostics;
+
+namespace ZamoraAlcivarExamenFinal.Services
+{
+    public class DatabaseService
+    {
+        SQLiteAsyncConnection Database;
+
+        public DatabaseService()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<Suscripcion>();
+        }
+
+        public async Task<List<Suscripcion>> GetSuscripcionesAsync()
+        {
+            await Init();
+            return await Database.Table<Suscripcion>().ToListAsync();
+        }
+
+        public async Task<int> SaveSuscripcionAsync(Suscripcion suscripcion)
+        {
+            await Init();
+            return await Database.InsertAsync(suscripcion);
+        }
+    }
+
+    public static class Constants
+    {
+        public const string DatabaseFilename = "Suscripciones.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath =>
+            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
---
+++ ZamoraAlcivarExamenFinal/Services/LogService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZamoraAlcivarExamenFinal.Services
+{
+    public class LogService
+    {
+        private readonly string _logFilePath;
+
+        public LogService()
+        {
+            string fileName = $"Logs_Zamora.txt";
+            _logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
+        }
+
+        public async Task RegistrarLog(string entry)
+        {
+            using StreamWriter writer = new StreamWriter(_logFilePath, append: true);
+            await writer.WriteLineAsync(entry);
+        }
+
+        public async Task<List<string>> ObtenerLogs()
+        {
+            if (!File.Exists(_logFilePath))
+                return new List<string>();
+
+            return (await File.ReadAllLinesAsync(_logFilePath)).ToList();
+        }
+    }
+}
